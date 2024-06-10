a4f2c933b1c3c6b7a3c16f087c2c10dd75e803a6
DokuaAsiedu <asiedudokua@gmail.com>
2024-06-10 01:05:37 +0000

    feat: added body parser middleware

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 import { PORT } from "./config/config.js"
 import express from "express"
+import bodyParser from "body-parser"
 import { hospitalsRouter, bedsRouter, usersRouter, patientsRouter } from "./routes/index.js"
 
 const app = express()
 
 app.use(express.json())
+app.use(bodyParser.urlencoded({extended: false}))
 
 app.listen(PORT || 8080, () => {
   console.log(`App up on port ${PORT}!`)
